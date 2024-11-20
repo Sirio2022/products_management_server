ec0bd5f33bc7d2c3ac74cd6fbc3bfa2c28b9dfd9
Juan Manuel Alvarez Bermudez <105917953+Sirio2022@users.noreply.github.com>
2024-11-20 15:24:00 -0500

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,8 +2,21 @@ import { CorsOptions } from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 export const corsConfig: CorsOptions = {
-  origin: 'https://products-management-client.vercel.app',
-  optionsSuccessStatus: 200,
+  origin: function (origin, callback) {
+    //console.log(origin); // Here we can see the origin of the request. In this case, it's the postman request. And the origin is undefined.
+
+    const whiteList = [process.env.FRONTEND_URL as string];
+
+    if (process.argv[2] === '--api') {
+      // This is a way to check if we are in development mode or production mode. If we are in development mode, we will add undefined to the whiteList array.
+      whiteList.push(undefined);
+    }
+
+    if (whiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
 };
