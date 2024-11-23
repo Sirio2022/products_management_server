0ba739dced146ba77b12d6720c639f0ba058ae8b
Juan Manuel Alvarez Bermudez <105917953+Sirio2022@users.noreply.github.com>
2024-11-23 09:34:35 -0500

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const corsConfig: CorsOptions = {
-  origin: process.env.CORS_ORIGIN,
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
