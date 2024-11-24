523bf8d1f9eadb19933d2da19fa13179582446c0
Juan Manuel Alvarez Bermudez <105917953+Sirio2022@users.noreply.github.com>
2024-11-24 11:02:43 -0500

    mmmm

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,10 @@
 import { Sequelize } from 'sequelize-typescript';
 import dotenv from 'dotenv';
-import 'reflect-metadata'; // Add this line. It's required by sequelize-typescript
-import pg from 'pg';
 
 dotenv.config();
 
 const db = new Sequelize(process.env.POSTGRESQL_URI!, {
   dialect: 'postgres',
-  dialectModule: pg,
   protocol: 'postgres',
   logging: false,
   models: [__dirname + '/../models/**/*.model.ts'],
